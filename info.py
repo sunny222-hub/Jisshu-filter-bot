c8274d2777bcb5b194df53e1ff7356ec79b9f98c
sunny222-hub <sunnitaghosh2004@gmail.com>
2025-01-29 15:41:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gandudotcom9955:<db_8iZNxeXulrx9qly4>@cluster0.66uej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'gandudotcom9955')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
